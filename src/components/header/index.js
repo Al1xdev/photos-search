7eb26e853eb696aad5efc9dd140ff3d32d011e9c
Alex <alex.horn.98@mail.ru>
2021-05-15 17:06:31 +0600

    feat: add button back to home

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,11 +4,15 @@ import { NavLink } from 'react-router-dom';
 import './header.css';
 
 import favorites from '../../assets/img/bookmark.svg';
+import home from '../../assets/img/home.svg';
 
 function Header() {
   return (
     <div className="header">
-      <NavLink to="/favorite" className="link">
+      <NavLink to="/">
+        <img src={home} alt="go home" />
+      </NavLink>
+      <NavLink to="/favorit" className="link">
         <img src={favorites} className="icon-saved" alt="favorites" />
       </NavLink>
     </div>
