0284ccf75695a90adbf35632c724e558ae95242d
Alex <alex.horn.98@mail.ru>
2021-06-07 17:28:39 +0600

    add axios header

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,33 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.unsplash.com'
+const BASE_URL = 'https://api.unsplash.com';
 
 export const getPhotos = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/photos?per_page=${20}&client_id=${process.env.REACT_APP_API_KEY}`);
+    const response = await axios.get(
+      `${BASE_URL}/photos?per_page=${20}&client_id=${process.env.REACT_APP_API_KEY}`,
+      {
+        headers: { 'Accept-Version': 'v1' },
+      },
+    );
     return response;
   } catch (error) {
-    console.log("Something went wrong"+error);
+    console.log('Something went wrong' + error);
   }
-}
+};
 
 export const getSearchPhotos = async (query) => {
   try {
-    const response = await axios.get(`${BASE_URL}/search/photos/?query=${query}&per_page=${20}&client_id=${process.env.REACT_APP_API_KEY}`);
+    const response = await axios.get(
+      `${BASE_URL}/search/photos/?query=${query}&per_page=${20}&client_id=${
+        process.env.REACT_APP_API_KEY
+      }`,
+      {
+        headers: { 'Accept-Version': 'v1' },
+      },
+    );
     return response;
   } catch (error) {
-    console.log("Something went wrong"+ error)
+    console.log('Something went wrong' + error);
   }
-}
+};
