d77158faba64e7496a0536d4027304884c110cf1
Alex <alex.horn.98@mail.ru>
2021-06-08 18:58:17 +0600

    fix cors

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/header';
 import Favorite from './pages/favorite';
 import Home from './pages/home';
+import History from './pages/history';
 
 import './App.css';
 
 function App() {
   const [favorit, setFavorit] = useState([]);
+  const [search, setSearch] = useState('');
+  const [historyItems, setHistoryItems] = useState([]);
 
   return (
     <div className="container">
@@ -19,13 +22,23 @@ function App() {
             <Route
               exact
               path="/"
-              render={() => <Home favorit={favorit} setFavorit={setFavorit} />}
+              render={() => (
+                <Home
+                  favorit={favorit}
+                  setFavorit={setFavorit}
+                  search={search}
+                  setSearch={setSearch}
+                  historyItems={historyItems}
+                  setHistoryItems={setHistoryItems}
+                />
+              )}
             />
             <Route
               exact
               path="/favorit"
               render={() => <Favorite favorit={favorit} setFavorit={setFavorit} />}
             />
+            <Route exact path="/history" render={() => <History search={search} historyItems={historyItems}/>} />
           </Switch>
         </Router>
       </div>
